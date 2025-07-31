e13a99daba13077c825ee121bb30f6c79b763771
Sachin Rupani <sachin.rupani1992@gmail.com>
2025-07-31 16:03:05 +0530

    Removed an unused function

--- src/app/utils/UtilFunctions.ts
+++ src/app/utils/UtilFunctions.ts
@@ -1,7 +1,3 @@
 export const isEmptyString = (value: string): boolean => {
   return value.trim().length < 1;
 };
-
-export const isEmptyArray = (array: Array<any>): boolean => {
-  return array.length < 1;
-};
